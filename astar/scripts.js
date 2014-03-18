cddfe553be36f5d11fd52e752890d433605d69d1
madslundt <madssx@hotmail.com>
2014-03-18 19:26:06 +0100

    Forgot to closePath and added mouse detection.

--- astar/scripts.js
+++ astar/scripts.js
@@ -99,7 +99,29 @@ var astar = {
 };
 
 var draw = {
-    drawArrow: function(ctx, fromPoint, toPoint) {
+    infobar: function(canvas, incX, incY, x_min, y_min, add, radius) {
+        canvas.addEventListener('mousemove', function(evt) {
+            var mousePos = draw.getMousePos(canvas, evt);
+            var message = 'X: ' + parseInt((mousePos.x / incX) + x_min - add - (radius / incX * 2)) + ', Y: ' + parseInt((mousePos.y / incY) + y_min - add - (radius / incY * 2));
+            $('.infobar .position').text(message);
+        }, false);
+
+        canvas.addEventListener('click', function(evt) {
+            var mousePos = draw.getMousePos(canvas, evt);
+            var message = 'X: ' + parseInt((mousePos.x / incX) + x_min - add - (radius / incX * 2)) + ', Y: ' + parseInt((mousePos.y / incY) + y_min - add - (radius / incX * 2));
+            $('.infobar .clickposition').text(message);
+        }, false);
+
+        $('.infobar .info').text(((map.length + 1) + ' nodes'));
+    },
+    getMousePos: function(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top
+        };
+    },
+    drawArrow: function(context, fromPoint, toPoint) {
         var dx = toPoint.x - fromPoint.x;
         var dy = toPoint.y - fromPoint.y;
 
@@ -112,28 +134,31 @@ var draw = {
 
 
         // Drawing Arrow Line.
-        ctx.beginPath();
-        ctx.moveTo(fromPoint.x, fromPoint.y);
-        ctx.lineTo(toPoint.x, toPoint.y);
-        ctx.closePath();
-        ctx.lineWidth = 1;
-        ctx.stroke();
+        context.beginPath();
+        context.moveTo(fromPoint.x, fromPoint.y);
+        context.lineTo(toPoint.x, toPoint.y);
+        context.closePath();
+        context.lineWidth = 1;
+        context.stroke();
 
-        ctx.moveTo(toPoint.x, toPoint.y);
-        ctx.lineTo((toPoint.x - unitDx * arrowHeadSize - unitDy * arrowHeadSize), (toPoint.y - unitDy * arrowHeadSize + unitDx * arrowHeadSize));
-        ctx.lineTo((toPoint.x - unitDx * arrowHeadSize + unitDy * arrowHeadSize), (toPoint.y - unitDy * arrowHeadSize - unitDx * arrowHeadSize));
-        ctx.lineTo(toPoint.x, toPoint.y);
-        ctx.fillStyle = '#000';
-        ctx.fill();
-        ctx.stroke();
+        context.moveTo(toPoint.x, toPoint.y);
+        context.lineTo((toPoint.x - unitDx * arrowHeadSize - unitDy * arrowHeadSize), (toPoint.y - unitDy * arrowHeadSize + unitDx * arrowHeadSize));
+        context.lineTo((toPoint.x - unitDx * arrowHeadSize + unitDy * arrowHeadSize), (toPoint.y - unitDy * arrowHeadSize - unitDx * arrowHeadSize));
+        context.lineTo(toPoint.x, toPoint.y);
+        context.fillStyle = '#000';
+        context.fill();
+        context.stroke();
 
-        /*ctx.moveTo((toPoint.x - unitDx * arrowHeadSize - unitDy * arrowHeadSize), (toPoint.y - unitDy * arrowHeadSize + unitDx * arrowHeadSize));
-        ctx.lineTo(toPoint.x, toPoint.y);
-        ctx.lineTo((toPoint.x - unitDx * arrowHeadSize + unitDy * arrowHeadSize), (toPoint.y - unitDy * arrowHeadSize - unitDx * arrowHeadSize));
-        ctx.stroke();*/
+
+        /*context.moveTo((toPoint.x - unitDx * arrowHeadSize - unitDy * arrowHeadSize), (toPoint.y - unitDy * arrowHeadSize + unitDx * arrowHeadSize));
+        context.lineTo(toPoint.x, toPoint.y);
+        context.lineTo((toPoint.x - unitDx * arrowHeadSize + unitDy * arrowHeadSize), (toPoint.y - unitDy * arrowHeadSize - unitDx * arrowHeadSize));
+        context.stroke();*/
+        context.closePath();
     },
     drawMap: function(canvas, map) {
         var context = canvas.getContext('2d');
+
         var radius = 5;
         context.clearRect(0, 0, canvas.width, canvas.height);
         // Get max
@@ -150,20 +175,24 @@ var draw = {
         }
         var incX = canvas.width / x_max;
         var incY = canvas.height / y_max;
+        var add = 5;
+        this.infobar(canvas, incX, incY, x_min, y_min, add, radius);
         for (var i = 0; i < map.length; i++) {
 
             var x1 = ((map[i].start[0] > 0) ? (map[i].start[0] - x_min) : 1);
             var y1 = ((map[i].start[1] > 0) ? (map[i].start[1] - y_min) : 1);
             var x2 = ((map[i].end[0] > 0) ? (map[i].end[0] - x_min) : 1);
             var y2 = ((map[i].end[1] > 0) ? (map[i].end[1] - y_min) : 1);
-            x1 += 5;
-            x2 += 5;
-            y1 += 5;
-            y2 += 5;
+
+            x1 += add;
+            x2 += add;
+            y1 += add;
+            y2 += add;
             x1 *= incX;
             x2 *= incX;
             y1 *= incY;
             y2 *= incY;
+
             var centerX = ((parseInt(x1) + parseInt(x2)) / 2);
             var centerY = (parseInt(y1) + parseInt(y2)) / 2;
 
@@ -173,14 +202,9 @@ var draw = {
             context.arc(x2, y2, radius, 0, 2 * Math.PI, false);
             context.fillStyle = 'black';
             context.fill();
+            context.closePath();
 
-            // Drawing lines / edges
-            context.beginPath();
-            context.moveTo(x1, y1);
-            context.lineTo(x2, y2);
-            context.stroke();
-
-            // Arrow head
+            // Drawing lines / edges with arrow head
             var ax = x2 - (radius - 1);
             var ay = y2 - (radius - 1);
             if (x1 > x2) {
@@ -198,10 +222,12 @@ var draw = {
             });
 
             // Setting text / street name
+            context.beginPath();
             context.textAlign = 'center';
             context.fillStyle = '#000';
-
+            context.font = '10pt Arial';
             context.fillText(map[i].name, centerX, centerY);
+            context.closePath();
         }
     }
 };
