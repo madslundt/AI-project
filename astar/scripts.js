27fcbd4d35d3f3e658ad6804fb016c04de132d52
madslundt <madssx@hotmail.com>
2014-03-19 01:53:07 +0100

    Path finding with a star implemented

--- astar/scripts.js
+++ astar/scripts.js
@@ -3,11 +3,23 @@ var map = [];
 $(function() {
     var canvas = document.getElementById('mapCanvas');
     $('#submit').click(function() {
-        parseMap($('#map').val());
+        map = parse.parseMap($('#map').val());
     });
     $('#draw').click(function() {
         draw.drawMap(canvas, map);
     });
+  
+    $('#findPath').click(function() {
+        var start = document.getElementById('start_select');
+        var startval = start.options[start.selectedIndex].value;
+        console.log('From: ' + map[startval].start);
+      
+        var goal = document.getElementById('goal_select');
+        var goalval = goal.options[goal.selectedIndex].value;
+        console.log('To: ' + map[goalval].start);
+        astar.search(map[startval].start, map[goalval].start, map, true);
+    });
+  
     canvas.width = document.getElementById('container').clientWidth;
     canvas.height = document.getElementById('container').clientHeight;
 });
@@ -23,7 +35,7 @@ $(function() {
             20
         ],
         "name": "Vesterbrogade",
-        "end": [
+        "goal": [
             30,
             40
         ]
@@ -34,7 +46,7 @@ $(function() {
             20
         ],
         "name": "Vesterbrogade",
-        "end": [
+        "goal": [
             30,
             30
         ]
@@ -43,36 +55,56 @@ $(function() {
  */
 
 
-function parseMap(text) {
-    var split = text.split('\n');
-    var ret_json = [];
-    $.each(split, function(key, value) {
-        var svalue = value.split(' ');
-
-        // Check for robustness.
-        // Coordinates have to be integers.
-        if (svalue.length == 5 &&
-            svalue[0] == parseInt(svalue[0]) && svalue[1] == parseInt(svalue[1]) &&
+var parse = {
+    parseMap: function(text) {
+      var split = text.split('\n');
+      var ret_json = [];
+      for (var i = 0; i < split.length; i++)
+      {
+          var svalue = split[i].split(' ');
+          
+          // Check for robustness.
+          // Coordinates have to be integers.
+          if (svalue.length == 5 &&
+              svalue[0] == parseInt(svalue[0]) && svalue[1] == parseInt(svalue[1]) &&
             svalue[3] == parseInt(svalue[3]) && svalue[4] == parseInt(svalue[4])) {
-            var street = {
+              var street = {
+                "id": i,
                 "start": [svalue[0], svalue[1]],
                 "name": svalue[2],
                 "end": [svalue[3], svalue[4]]
+              }
+              // Add coordinates to the street.
+              ret_json.push(street);
             }
-            // Add coordinates to the street.
-            ret_json.push(street);
+      }
+      this.setSelector(ret_json);
+      return ret_json;
+    },
+    setSelector: function(map) {
+        $('.start_select').html('');
+        $('.goal_select').html('');
+        for (var i = 0; i < map.length; i++) {
+            $('.start_select').append('<option value="' + i + '">(' + map[i].start[0] + ', ' + map[i].start[1] + ')</option>');
+            $('.goal_select').append('<option value="' + i + '">(' + map[i].start[0] + ', ' + map[i].start[1] + ')</option>');
         }
-    });
-    this.map = ret_json;
-}
+        if (map.length < 1) {
+            $('.start_select').html('<option>No nodes</option>');
+            $('.goal_select').html('<option>No nodes</option>');
+        }
+    }
+};
 
 var astar = {
-    init: function(map) {
+    init: function(start, map) {
+        var ret_index = -1;
         for (var i = 0; i < map.length; i++) {
-            map[i].g = 0;
-            map[i].f = 0;
-            map[i].h = 0;
+            if (map[i].start == start) {
+                ret_index = i;
+            }
         }
+      
+        return ret_index;
     },
     /**
      * [search description]
@@ -80,21 +112,131 @@ var astar = {
      * @param  [x, y] end   [description]
      * @param  [map] map   [description]
      */
-    search: function(start, end, map) {
-        astar.init(map);
-
+    search: function(start, goal, map, debug) {
+        
+        var start_index = astar.init(start, map);
+        if (start_index == -1) {
+            console.log('error');
+            return;
+        }
+        
         var closedset = [];
         var openset = [];
-        var came_from = [];
+        var node = {
+          "node": start,
+          "g": 0,
+          "f": this.heuristic(start, goal)
+        }
+        openset.push(node);
+        
+        var current_path = [];
+        var ret = false;
+        var count = 0;
+        var current;
+        var currentIndex = 0;
+        while (openset.length > 0) {
+            current = openset[0];
+            for (var i = 0; i < openset.length; i++) { // Finding lowest f value in openset.
+                if (openset[i].f < current.f) {
+            
+                  current = openset[i];
+                  currentIndex = i;
+                }
+            }
+            console.log(current);
+            // Checking if we are at the goal
+            if (current.node[0] == goal[0] && current.node[1] == goal[1]) {
+                // Maybe draw to the map
+                current_path.push(current);
+                ret = true;
+                break;
+            }
+            
+            // Remove current from openset.
+            openset.splice(openset[currentIndex], 1);
+            closedset.push(current);
+          
+            var neighbors = this.neighbors(current, map);
+            for (i = 0; i < neighbors.length; i++) {
+                if (this.isNodeInList(neighbors[i].node, closedset)) {
+                    if (debug) {
+                        console.log("Neighbor is already visited");
+                    }
+                    continue;
+                }
+                var tentative_g_score = current.g + this.distanceBetween(current, neighbors[i]);
+
+                if (!this.isNodeInList(neighbors[i].node, openset) || tentative_g_score < neighbors[i].g) { // If neighbor is not in openset
+                    current_path.push(current);
+                    neighbors[i].g = tentative_g_score;
+                    neighbors[i].f = neighbors[i].g + this.heuristic(neighbors[i].node, goal);
+                    
+                    if (!this.isNodeInList(neighbors[i].node, openset)) {
+                        if (debug) {
+                            console.log(neighbors[i] + ' pushed to openset'); 
+                        }
+                        openset.push(neighbors[i]);
+                    }
+                }
+            }
+            if (debug) {
+              console.log("Node: (" + current.node[0] + ", " + current.node[1] + ") with G: " + current.g + " - F: " + current.f);
+            }
+            if (count++ >= 100)
+                break;
+        } // End while
+          
+        if (ret) {
+            console.log("Path found");
+            if (debug) {
+                console.log(current_path);       
+            }
+            draw.drawRoute(current_path, map);
+        } else {
+            console.log("Program terminated. Could not find a path");
+        }
+                      
+        
     },
     neighbors: function(node, map) {
         var ret_neighbors = [];
+        if (map.length < 1) {
+            return ret_neighbors;
+        }
         for (var i = 0; i < map.length; i++) {
-            if (map[i].start == node) {
-                ret_neighbors.push(map[i]);
+            if (map[i].start[0] == node.node[0] && map[i].start[1] == node.node[1]) { // If start equals the node, the map.end node must be a neighbor.
+                ret_neighbors.push({
+                  "node": map[i].end, 
+                  "g": (typeof node.g == 'undefined' ? 0 : node.g), 
+                  "f": (typeof node.f == 'undefined' ? 0 : node.f)
+                });
             }
         }
         return ret_neighbors;
+    },
+    /**
+    * Manhatten
+    */
+    heuristic : function(start, goal) {
+        var d1 = Math.abs(start[0] - goal[0]);
+        var d2 = Math.abs(start[1] - goal[1]);
+      
+        return d1 + d2;
+    },
+    distanceBetween: function(start, goal) {
+      var x = Math.abs(start.node[0] - goal.node[0]);
+      var y = Math.abs(start.node[1] - goal.node[1]);
+      
+      return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+      
+    },
+    isNodeInList: function(node, list) {
+        for (var i = 0; i < list.length; i++) {
+            if (list[i].node[0] == node[0] && list[i].node[1] == node[1]) {
+              return true;
+            }
+        }
+        return false;
     }
 };
 
@@ -137,6 +279,7 @@ var draw = {
         context.beginPath();
         context.moveTo(fromPoint.x, fromPoint.y);
         context.lineTo(toPoint.x, toPoint.y);
+        context.fillStyle = '#000';
         context.closePath();
         context.lineWidth = 1;
         context.stroke();
@@ -229,5 +372,61 @@ var draw = {
             context.fillText(map[i].name, centerX, centerY);
             context.closePath();
         }
+    },
+    drawRoute: function(path, map) {
+        console.log(map);
+        var canvas = document.getElementById('mapCanvas');
+        var context = canvas.getContext('2d');
+        var radius = 5;
+        // Get max
+        var x_max = 0;
+        var y_max = 0;
+        var x_min = map[0].start[0];
+        var y_min = map[0].start[1];
+
+        for (var i = 0; i < map.length; i++) {
+            x_max = Math.max(map[i].start[0], map[i].end[0], x_max);
+            y_max = Math.max(map[i].start[1], map[i].end[1], y_max);
+            x_min = Math.min(map[i].start[0], map[i].end[0], x_min);
+            y_min = Math.min(map[i].start[1], map[i].end[1], y_min);
+        }
+        var incX = canvas.width / x_max;
+        var incY = canvas.height / y_max;
+        var add = 5;
+        for (i = 0; i < path.length - 1; i++) {
+            var x1 = ((path[i].node[0] > 0) ? (path[i].node[0] - x_min) : 1);
+            var y1 = ((path[i].node[1] > 0) ? (path[i].node[1] - y_min) : 1);
+            var x2 = ((path[i+1].node[0] > 0) ? (path[i+1].node[0] - x_min) : 1);
+            var y2 = ((path[i+1].node[1] > 0) ? (path[i+1].node[1] - y_min) : 1);
+
+            x1 += add;
+            y1 += add;
+            x2 += add;
+            y2 += add;
+          
+            x1 *= incX;
+            y1 *= incY;
+            x2 *= incX;
+            y2 *= incY;
+
+           
+            // Drawing lines / edges with arrow head
+            var ax = x2 - (radius - 1);
+            var ay = y2 - (radius - 1);
+            if (x1 > x2) {
+                ax = x2 + (radius - 1);
+            }
+            if (y1 > y2) {
+                ay = y2 + (radius - 1);
+            }
+            context.beginPath();
+            context.moveTo(x1, y1);
+            context.lineTo(ax, ay);
+            context.lineWidth = 3;
+            context.strokeStyle = '#b00';
+            context.stroke();
+            context.closePath();
+        }
+  
     }
 };
